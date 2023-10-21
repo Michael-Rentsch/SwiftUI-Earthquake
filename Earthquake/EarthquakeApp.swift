a495704a80fea72313069bb691e1954772d00a9f
Michael Rentsch <64227211+Michael-Rentsch@users.noreply.github.com>
2023-10-21 14:42:46 +0200

    Initial Commit

---
+++ Earthquake/EarthquakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakeApp.swift
+//  Earthquake
+//
+//  Created by Michael Rentsch on 2023/10/21.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
